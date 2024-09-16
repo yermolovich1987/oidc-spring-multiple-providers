8d267e03e8957e2aa9accf5d562c089d95046a69
Dmytro Iermolovych <yermolovich1987@gmail.com>
2024-09-16 08:26:56 +0200

    Temp commit

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -2,39 +2,16 @@ package com.example.demo;
 
 import com.example.demo.dynamic.DynamicOAuth2ClientRegistrationRepository;
 import com.example.demo.dynamic.IdentityProviderService;
-import com.example.demo.dynamic.SimpleInMemoryIdentityProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import java.util.function.Consumer;
 
 @Configuration
 public class SecurityConfiguration {
-
-//    @Autowired
-//    private ClientRegistrationRepository clientRegistrationRepository;
-
-//  @Bean
-//  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-//    http
-//        .authorizeExchange()
-//        .anyExchange()
-//        .authenticated()
-//        .and()
-//        .oauth2Login(); // to redirect to oauth2 login page.
-//
-//    return http.build();
-//  }
 
     @Bean
     public DynamicOAuth2ClientRegistrationRepository dynamicClientRegistrationRepository(
@@ -46,35 +23,16 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http,
                                            DynamicOAuth2ClientRegistrationRepository dynamicOAuth2ClientRegistrationRepository) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/idp/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                // This is required to execute POST requests without CSRF tokens
+                .csrf(AbstractHttpConfigurer::disable)
 //                .oauth2Login(Customizer.withDefaults());
                 .oauth2Login(oauth2 -> oauth2
-                                .clientRegistrationRepository(dynamicOAuth2ClientRegistrationRepository)
-//                        .authorizationEndpoint(authorization -> authorization
-//                                .authorizationRequestResolver(
-//                                        authorizationRequestResolver(this.clientRegistrationRepository)
-//                                )
-//                        )
+                        .clientRegistrationRepository(dynamicOAuth2ClientRegistrationRepository)
                 );
 
         return http.build();
     }
-
-//    private OAuth2AuthorizationRequestResolver authorizationRequestResolver(
-//            ClientRegistrationRepository clientRegistrationRepository) {
-//
-//        DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver =
-//                new DefaultOAuth2AuthorizationRequestResolver(
-//                        clientRegistrationRepository, "/oauth2/authorization");
-//        authorizationRequestResolver.setAuthorizationRequestCustomizer(
-//                authorizationRequestCustomizer());
-//
-//        return  authorizationRequestResolver;
-//    }
-//
-//    private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
-//        return customizer -> customizer
-//                .additionalParameters(params -> params.put("prompt", "consent"));
-//    }
 }
--- src/main/java/com/example/demo/dynamic/ClientRegistrationController.java
+++ src/main/java/com/example/demo/dynamic/ClientRegistrationController.java
@@ -1,14 +1,18 @@
 package com.example.demo.dynamic;
 
+import com.example.demo.dynamic.dto.IdpDetails;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrations;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,24 +21,40 @@ import java.util.UUID;
 public class ClientRegistrationController {
     private final IdentityProviderService identityProviderService;
 
-    @PostMapping
-    void registerNewIdp() {
-//        ClientRegistration clientRegistration = ClientRegistration
-//                .withRegistrationId(UUID.randomUUID().toString())
-//                .clientName("testPing")
-//                .build();
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    void registerNewIdp(@RequestBody IdpDetails idpDetails) {
         var registration = ClientRegistrations
-                .fromOidcIssuerLocation("https://auth.pingone.eu/fd252206-ed6e-4760-b0d6-ec4fcbeb040e/as")
-                .clientName("testDynamicPing")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .fromOidcIssuerLocation(idpDetails.getIssuerUri())
+                .clientName(idpDetails.getClientName())
+                .clientId(idpDetails.getClientId())
+                .clientSecret(idpDetails.getClientSecret())
+                .clientAuthenticationMethod(idpDetails.getClientAuthenticationMethod())
                 .build();
 
 
-        identityProviderService.upsertIdpRegistration("testCompany", registration);
+        identityProviderService.upsertIdpRegistration(idpDetails.getCompanyName(), registration);
     }
 
-    @PostConstruct
-    void postConstruct() {
-        registerNewIdp();
+    @PostMapping(path = "/batch", consumes = "application/json", produces = "application/json")
+    void registerBatchOfIdps(@RequestBody List<IdpDetails> idpDetailList) {
+        idpDetailList.forEach(this::registerNewIdp);
     }
+
+    @GetMapping(produces = "application/json")
+    List<PerCompanyRegistration> getAllRegisteredIdps() {
+        return identityProviderService.getAllIdpRegistrations();
+    }
+
+//    @PostConstruct
+//    void postConstruct() {
+//        var registration = ClientRegistrations
+//                .fromOidcIssuerLocation("https://auth.pingone.eu/fd252206-ed6e-4760-b0d6-ec4fcbeb040e/as")
+//                .clientName("testDynamicPing")
+//                .clientId("df78ef33-896d-411d-bd96-1d7580e4af3d")
+//                .clientSecret("iwIcjJk69J3OEftWDK0HuHMrd44H8jlGcUoyZWvbQmGCuU8widWo9vueLln6GGg.")
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+//                .build();
+//
+//        registerNewIdp();
+//    }
 }
